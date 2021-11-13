f65f52ed6bfe945c1b26d51e9d7a062e89b043d6
mooben <18700488+mooben@users.noreply.github.com>
2021-11-13 14:36:10 +0100

    ...

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
@@ -5,6 +7,36 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+            
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int i = 0;
+            int a,b,c,d,e,f = 0;
+            foreach (var student in Students)
+            {
+                i++;
+
+                switch (student.Grades)
+                {
+                    
+                }
+            }
+
+            if (i < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to works");
+            }
+            else
+            {
+                switch (Type)
+                {
+                    
+                }
+                return 'F';
+            }
+            
         }
     }
 }
